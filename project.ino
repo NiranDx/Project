cfc3069f8578154a6e04844f9496ed0de4353e04
NiranDx <niran3688@gmail.com>
2019-02-04 01:04:18 +0700

    add lcd

--- project.ino
+++ project.ino
@@ -2,6 +2,8 @@
 #include <MFRC522.h>
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h> ///ดึง LCD มาใช้
 //ระบุชื่อ Wi-fi ที่จะต่อ
 #define WIFI_SSID "N~Alzheimer~N"
 #define WIFI_PASSWORD "21052539."
@@ -9,30 +11,33 @@
 #define FIREBASE_HOST "uidofrfid.firebaseio.com"
 #define FIREBASE_AUTH "iE3hL21pDJQn5MPw5uC20MATTneWzac97s7879Go"
 //กำหนดขา RFID
-#define SS_PIN D2
-#define RST_PIN D1
+#define SS_PIN D4
+#define RST_PIN D3
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 #define Buz D0
+LiquidCrystal_I2C lcd(0x3F, 16, 2); //สร้างตัวแปร lcd
 
 void setup() { 
   pinMode(Buz,OUTPUT);
   Serial.begin(115200);
   SPI.begin(); //ให้เริ่มทำงาน SPI
   mfrc522.PCD_Init(); //rfid เริ่มทำงาน
- 
+  lcd.begin(); // lcd เริ่มทำงาน
   
    // เชื่อม wifi.
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  
+  lcd.print("Connecting"); //Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
+    lcd.print("."); //Serial.print(".");
     delay(500);
   }
   Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Connected "); //Serial.print("connected: ");
+  lcd.setCursor(0,1);
+  lcd.print(WiFi.localIP()); //Serial.println(WiFi.localIP());
   //เชื่อม Firebase
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   digitalWrite(Buz, HIGH), delay(100), digitalWrite(Buz, LOW);
@@ -52,7 +57,9 @@ void loop() {
     return;
   }
   Serial.println();
-  Serial.print(" UID tag :");
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" UID tag :"); //Serial.print(" UID tag :");
   String content= "";
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
@@ -64,6 +71,8 @@ void loop() {
   }
   content.toUpperCase();
   Serial.println();  ///รหัส UID อยู๋ในตัวแปร Content แล้ว
+  lcd.setCursor(0,1);
+  lcd.print(content);
 
   digitalWrite(Buz, HIGH), delay(200), digitalWrite(Buz, LOW);
   delay(2000);
@@ -71,11 +80,15 @@ void loop() {
   ///ส่งรหัสไป Firebase
   String name = Firebase.pushString("Customer", content);
    if (Firebase.failed()) {
-      Serial.print("pushing /Customer failed:");
+      lcd.clear();
+      lcd.print("pushing /Customer failed:");
+      //Serial.print("pushing /Customer failed:");
       Serial.println(Firebase.error());  
       return;
   }
-  Serial.print("pushed: /Customer/");
+  lcd.clear();
+  lcd.print("pushed: /Customer/");
+  //Serial.print("pushed: /Customer/");
   Serial.println(name);
 
   mfrc522.PICC_HaltA();
